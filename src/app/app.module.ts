4cb3ceb99a50bdaa090ee24d6f0d1287943b5c97
Jesus Alvarado <112035392+jesusas99@users.noreply.github.com>
2023-04-18 08:20:47 -0700

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PostComponent } from './post/post.component'
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    PostComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
